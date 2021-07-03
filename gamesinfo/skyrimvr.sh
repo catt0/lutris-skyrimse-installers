067687ddb619ab284f56db3eaa0d4e0df8cbbd25
catt0 <catt0@catt0.de>
2021-07-03 20:38:50 +0200

    Add Skyrim VR

---
+++ gamesinfo/skyrimvr.sh
@@ -0,0 +1,7 @@
+game_steam_subdirectory="SkyrimVR"
+game_appid=611670
+game_proton_options="--native 'xaudio2_7' --protonver 5.0 -e 'PULSE_LATENCY_MSEC=90'"
+game_wine_options="--native 'xaudio2_7'"
+game_protontricks=""
+game_winetricks=""
+
